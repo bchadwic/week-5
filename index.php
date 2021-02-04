856490da5ca643e345a08aaa8dc6b2215d4c577b
Ben Chadwick <bchadwick@mail.greenriver.edu>
2021-02-04 14:56:17 -0500

    added some variables

--- index.php
+++ index.php
@@ -11,8 +11,16 @@ require_once('vendor/autoload.php');
 $f3 = Base::instance();
 $f3->set('DEBUG', 3);
 
-$f3->route('GET /', function() {
+$f3->route('GET /', function($f3) {
     // echo "Adding index page";
+    $f3->set('username', 'jshmo');
+    $f3->set('password', sha1('Password01'));
+    $f3->set('title', 'Working with Templates');
+    $f3->set('ftemp', 67);
+    $f3->set('color', 'purple');
+    $f3->set('radius', 10);
+
+
     $view = new Template();
     echo $view->render('views/info.html');
 });
